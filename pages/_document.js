0eb6d19eec1ddfe80b870acbef7ade74246e2c12
Jeremiah Quill <jcq5010@gmail.com>
2022-12-31 12:55:38 -0500

    testing google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -7,7 +8,25 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M6MVT93');
+      `}
+        </Script>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<
+                      src="https://www.googletagmanager.com/ns.html?id=GTM-M6MVT93"
+                      height="0"
+                      width="0"
+                      style="display:none;visibility:hidden" />`,
+          }}
+        />
       </body>
     </Html>
-  )
+  );
 }
